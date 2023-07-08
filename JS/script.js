5a95a8437c02320fab37e793689ceecf2c48167d
Hellraiser997 <felipesg03@gmail.com>
2023-07-08 16:46:39 -0300

    Feat: validação formulário.

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function validaFormulario() {
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    var confirmacaoSenha = document.getElementById("confirmacaoSenha").value;
+    
+}
