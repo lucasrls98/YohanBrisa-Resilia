a078631a1a0afc96f764d839e8a7b94b80fc9c81
Hellraiser997 <felipesg03@gmail.com>
2023-07-08 16:51:46 -0300

    Feat: condicionais validação formulário

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,21 @@ function validaFormulario() {
     var senha = document.getElementById("senha").value;
     var confirmacaoSenha = document.getElementById("confirmacaoSenha").value;
     
+    if (nome === "") {
+        alert("Favor inserir seu nome.");
+        return false;
+    }
+
+    if (senha.length < 5) {
+        alert("A senha deve ter no mínimo 5 caracteres.");
+        return false;
+    }
+
+    if (confirmacaoSenha != senha) {
+        alert("A confirmação deve ser igual a senha.");
+        return false;
+    }
+
+    return true;
+    
 }
